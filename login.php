978ee0805da6c9dd5d3fa717d0227ad9a15dd192
Jerome Pepito <jerome.pepito2888@gmail.com>
2016-10-15 14:05:58 +0800

    new login validation

--- login.php
+++ login.php
@@ -1,10 +1,29 @@
+ <?php
+$errMSG="";
+if(count($_POST)>0) {
+$connect = mysql_connect("localhost","root","");
+mysql_select_db("roseph",$connect);
+$result = mysql_query("SELECT * FROM employee WHERE email='" . $_POST["email"] . "' and psswrd = '". $_POST["psw"]."'");
+$count  = mysql_num_rows($result);
+if($count==0) {
+$errMSG = "Invalid Username or Password!";
+//header("Location: login.php");
+
+echo $errMSG; 
+} else {
+$errMSG = "You are successfully authenticated!";
+header("Location: main.php");
+echo $errMSG; 
+}
+}
+?>
  <html>
  <head>
  <title>LOGIN</title>
  <link href="main.css" rel="stylesheet">
  </head>
  <body>
- <form action="lognpros.php" method="post" style="position:center;">
+ <form action="" method="post" style="position:center;">
  	  <label><b>Email Address</b></label> <br>
       <input type="text" placeholder="Enter Email Address" name="email" required> <br>
       <label><b>Password</b></label> <br>
